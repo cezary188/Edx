271eb876bed0de3ace459122f4a3a301d194066f
cezary188 <cezary188@gmail.com>
2018-07-23 22:24:15 +0100

    dodano variacje

--- name.py
+++ name.py
@@ -1,7 +1,7 @@
 print ("wpisz liczbe")
 liczba = input ()
 
-print (f"liczba to:{liczba}")
+
 
 
 
@@ -9,4 +9,4 @@ def kwadrat (x):
 	return x+x
 
 for i in range(10):
-	print("{}dodane do siebie to {}".format(i, kwadrat(i)))	
+	print("{}dodane do siebie to {} ale to co wpisales to {}".format(i, kwadrat(i), liczba))	
